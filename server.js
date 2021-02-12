a8dc9a2406b3426fd7ca121b96ad18972b83c071
Haramrit Khurana <haramrit09k@gmail.com>
2021-02-11 19:30:11 -0500

    comment mongo and socket code for heroku test

--- server.js
+++ server.js
@@ -15,59 +15,59 @@ const port = process.env.PORT || 4242;
 server.listen(port);
 console.debug('Server listening on port ' + port);
 
-// setup mongo and socket.io
-const mongo = require('mongodb').MongoClient;
-const client = require('socket.io')(server);
+// // setup mongo and socket.io
+// const mongo = require('mongodb').MongoClient;
+// const client = require('socket.io')(server);
 
-// mongo connection
-mongo.connect("mongodb://127.0.0.1/mongo-chat", function(err, db){
-    if(err){
-        throw err;
-    }
+// // mongo connection
+// mongo.connect("mongodb://127.0.0.1/mongo-chat", function(err, db){
+//     if(err){
+//         throw err;
+//     }
 
-    console.log("Connection to DB established!");
+//     console.log("Connection to DB established!");
 
-    // connection to socket.io
-    client.on('connection', function(socket){
-        let chat = db.collection('chats');
+//     // connection to socket.io
+//     client.on('connection', function(socket){
+//         let chat = db.collection('chats');
 
-        // send status across n/w
-        sendStatus = function(s) {
-            socket.emit('status', s);
-        }
+//         // send status across n/w
+//         sendStatus = function(s) {
+//             socket.emit('status', s);
+//         }
 
-        // fetch chats from db
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, response){
-            if(err){
-                throw err;
-            }
+//         // fetch chats from db
+//         chat.find().limit(100).sort({_id:1}).toArray(function(err, response){
+//             if(err){
+//                 throw err;
+//             }
 
-            socket.emit('messages', response);
-        });
+//             socket.emit('messages', response);
+//         });
 
-        socket.on('input', function(data){
-            let name = data.name;
-            let msg = data.message;
+//         socket.on('input', function(data){
+//             let name = data.name;
+//             let msg = data.message;
 
-            if(name == '' || msg == ''){
-                sendStatus('Name or message missing!');
-            }
-            else{
-                chat.insert({name: name, msg: msg}, function(){
-                    client.emit('messages', [data]);
+//             if(name == '' || msg == ''){
+//                 sendStatus('Name or message missing!');
+//             }
+//             else{
+//                 chat.insert({name: name, msg: msg}, function(){
+//                     client.emit('messages', [data]);
 
-                    sendStatus({
-                        message: 'Message successfully sent',
-                        clear: true
-                    });
-                });
-            }
-        });
+//                     sendStatus({
+//                         message: 'Message successfully sent',
+//                         clear: true
+//                     });
+//                 });
+//             }
+//         });
 
-        socket.on('clear', function(data){
-            chat.remove({}, function(){
-                socket.emit('cleared');
-            })
-        });
-    });
-});
+//         socket.on('clear', function(data){
+//             chat.remove({}, function(){
+//                 socket.emit('cleared');
+//             })
+//         });
+//     });
+// });
